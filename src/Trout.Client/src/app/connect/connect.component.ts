f1d1bf5af3d0d8b50212b7dc7235cf6016183868
Saravana Bhavan J <saravanabhavanj@gmail.com>
2017-01-23 09:16:19 +0530

    Add connect form

--- src/Trout.Client/src/app/connect/connect.component.ts
+++ src/Trout.Client/src/app/connect/connect.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnectModel } from './connect.model';
 
 @Component({
-  selector: 'app-connect',
-  templateUrl: './connect.component.html',
-  styleUrls: ['./connect.component.css']
+    selector: 'app-connect',
+    templateUrl: './connect.component.html',
+    styleUrls: ['./connect.component.css']
 })
 export class ConnectComponent implements OnInit {
 
-  constructor() { }
+    // Setup default values
+    private connectModel: ConnectModel = {
+        address: "irc.freenode.net:6667",
+        channels: "#trout",
+        enableSsl: true,
+        name: "trout-user",
+        nick: "trout-user",
+        realName: "trout-user"
+    };
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    connect(event: Event) {
+        debugger
+    }
 }
---
+++ src/Trout.Client/src/app/connect/connect.model.ts
@@ -0,0 +1,9 @@
+export class ConnectModel {
+    public name: string;
+    public address: string;
+    public password?: string;
+    public enableSsl: boolean;
+    public nick: string;
+    public realName: string;
+    public channels: string;
+}
